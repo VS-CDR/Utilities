ecef7c888da9ba16bd368f63aadaf895ffccb026
Vlad Shubin <shubin.va@phystech.edu>
2023-06-21 10:10:57 +0300

    small refactoring

--- Coders & Decoders/Hamming/CodeFixer.cpp
+++ Coders & Decoders/Hamming/CodeFixer.cpp
@@ -1,12 +1,15 @@
 //#include <windows.h>
+#include <algorithm>
 #include <string>
 #include <iostream>
 #include <vector>
 #include "../../fastmath.hpp"
 
-int FindMistake(std::string& input) {
+const int kBlockSize = 15;
+
+int FindMistake(std::string_view input) {
   int mistake = 0;
-  for (int i = 0; i < 15; ++i) {
+  for (int i = 0; i < kBlockSize; ++i) {
     if (input[i] == '1') {
       mistake ^= i + 1;
     }
@@ -14,8 +17,8 @@ int FindMistake(std::string& input) {
   return mistake;
 }
 
-bool CheckTheDigits(std::string& input) {
-  for (int i = 0; i < 15; ++i) {
+bool CheckTheDigits(std::string_view input) {
+  for (int i = 0; i < kBlockSize; ++i) {
     if (input[i] != '0' && input[i] != '1') {
       return false;
     }
@@ -23,11 +26,15 @@ bool CheckTheDigits(std::string& input) {
   return true;
 }
 
-void OutRes(int b, std::string& input, std::string& res) {
+void OutRes(std::string_view input, std::string& res) {
+  int bit = 1;
   std::string info;
-  for (int i = 0; i < 15; ++i) {
-    if (i + 1 == b) { b <<= 1; }
-    else { info += input[i]; }
+  for (int i = 0; i < kBlockSize; ++i) {
+    if (i + 1 == bit) {
+      bit <<= 1;
+    } else {
+      info += input[i];
+    }
   }
   std::cout << info << " ";
   res += info;
@@ -42,12 +49,12 @@ void Fix(int mistake, std::string& input) {
   }
 }
 
-void SplitInParts(std::vector<std::string>& parts, std::string& input) {
-  for (std::pair<std::size_t, int> iters(0, 0); iters.first < input.length();
-       iters.first += 15) {
-    std::string tmp(input, iters.first, 15);
-    parts[iters.second++] = tmp;
-    ++iters.first;
+void SplitInParts(std::vector<std::string>& parts, std::string_view input) {
+  for (auto [BlockBegin, BlockInd] = std::pair<std::size_t, int>(0, 0);
+       BlockBegin < input.length();
+       BlockBegin += kBlockSize) {
+    parts[BlockInd++] = std::string(input, BlockBegin, kBlockSize);
+    ++BlockBegin;
   }
 }
 
@@ -56,7 +63,7 @@ void PrepareCode(std::vector<std::string>& parts, std::string& input) {
   char c;
   std::cin >> c;
   if (c == 'n') {
-    for (size_t i = 15; i < input.length(); i += 16) {
+    for (size_t i = kBlockSize; i < input.length(); i += 16) {
       input.insert(i, " ");
     }
   }
@@ -74,32 +81,32 @@ int main() {
 
   std::cout << "Input your code: ";
 
-  int mistake = 0;
   std::string input;
-  std::string res;
   InputCode(input);
-  std::vector<std::string> parts(input.length() / 15);
+  std::vector<std::string> parts(input.length() / kBlockSize);
 
   PrepareCode(parts, input);
-  for (auto it = parts.begin(); it < parts.end(); ++it) {
-    mistake = FindMistake(*it);
-    if (CheckTheDigits(*it)) {
-      Fix(mistake, *it);
-      OutRes(1, *it, res);
+
+  std::string res;
+  for (auto& str : parts) {
+    int mistake = FindMistake(str);
+    if (CheckTheDigits(str)) {
+      Fix(mistake, str);
+      OutRes(str, res);
     } else {
       std::cout << "Invalid code";
     }
   }
 
-  std::cout << "\nSplit in eights(8): y/n?\n";
+  std::cout << '\n' << "Split in eights(8): y/n?" << '\n';
+
   char c;
   std::cin >> c;
-
   if (c == 'y') {
     for (std::size_t i = 8; i < res.length(); i += 9) {
       res.insert(i, " ");
     }
-    std::cout << "Output the result in file: y/n?\n";
+    std::cout << "Output the result in file: y/n?" << '\n';
     std::cin >> c;
     if (c == 'y') {
       freopen("res.txt", "w", stdout);
@@ -110,7 +117,7 @@ int main() {
     int pow = 7;
     int code = 0;
     while (res[i] != ' ' && i < res.length()) {
-      code += FastPow(2, pow--) * static_cast<int>(res[i++] - 48);
+      code += FastPow(2, pow--) * (res[i++] - '0');
     }
     std::cout << static_cast<unsigned char>(code);
   }
--- Coders & Decoders/Hamming/Encoder.cpp
+++ Coders & Decoders/Hamming/Encoder.cpp
@@ -4,41 +4,45 @@
 #include <vector>
 #include "../../fastmath.hpp"
 
-void AddExtraNulls(std::string& s) {
-  while (s.length() % 11) {
-    s.push_back('0');
+void AddExtraNulls(std::string& str) {
+  const int kInfoBits = 11;
+  while (str.length() % kInfoBits != 0) {
+    str.push_back('0');
   }
 }
 
-void RemoveUnusedSymbols(std::string& s) {
-  for (std::size_t i = 0; i < s.length(); ++i) {
-    if (!isdigit(s[i])) {
+void RemoveUnusedSymbols(std::string& str) {
+  for (std::size_t i = 0; i < str.length(); ++i) {
+    if (!isdigit(str[i])) {
       int cnt = 1;
-      while (!isdigit(s[++i])) {
+      while (!isdigit(str[++i])) {
         ++cnt;
       }
-      s.erase(i - cnt, cnt);
+      str.erase(i - cnt, cnt);
     }
   }
 }
 
-void AddControlBits(std::string& s) {
-  for (std::size_t iter = 1; iter < s.length(); iter*=2) {
-    s.insert(s.begin() + iter - 1, 'c');
+void AddControlBits(std::string& str) {
+  for (std::size_t iter = 1; iter < str.length(); iter *= 2) {
+    str.insert(str.begin() + iter - 1, 'c');
   }
 }
 
-void CalcControlBits(std::string& s) {
-  for (int step = 0; step <= 3; ++step) {
-    int c = FastPow(2, step);
+void CalcControlBits(std::string& str) {
+  const int kProtectBits = 4;
+  for (int step = 0; step < kProtectBits; ++step) {
+    int control_bit = FastPow(2, step);
     int cnt = 0;
-    for (std::size_t i = c + 1; i <= s.length(); ++i) {
-      if (i & c) {
-        cnt += s[i - 1] - '0';
+
+    for (std::size_t i = control_bit + 1; i <= str.length(); ++i) {
+      if (i & control_bit) {
+        cnt += str[i - 1] - '0';
       }
     }
 
-    s[static_cast<std::basic_string<char, std::char_traits<char>, std::allocator<char>>::size_type>(c) - 1] = static_cast<char>(48 + cnt % 2);
+    str[static_cast<std::size_t>(control_bit - 1)] =
+        static_cast<char>('0' + cnt % 2);
   }
 }
 
@@ -47,10 +51,11 @@ std::vector<std::string> ProcessSegments(std::vector<std::string> codes) {
   return codes;
 }
 
-std::vector<std::string> SplitInSegments(std::string_view s) {
+std::vector<std::string> SplitInSegments(std::string_view str) {
+  const int kInfoBits = 11;
   std::vector<std::string> segments;
-  for (std::size_t i = 0; i < s.length(); i += 11) {
-    segments.emplace_back(s.substr(i, 11));
+  for (std::size_t i = 0; i < str.length(); i += kInfoBits) {
+    segments.emplace_back(str.substr(i, kInfoBits));
     AddControlBits(segments.back());
   }
   return segments;
@@ -64,7 +69,7 @@ std::string VectorToStr(const std::vector<std::string>& codes) {
   return code;
 }
 
-int main(){
+int main() {
   std::ios::sync_with_stdio(false);
   std::string code;
 
@@ -77,5 +82,5 @@ int main(){
   std::vector<std::string> codes = ProcessSegments(SplitInSegments(code));
 
   code = VectorToStr(codes);
-  std::cout <<"Your code: "<< code;
+  std::cout << "Your code: " << code;
 }
