16b83c1c8451b3cd7666a5dbf5c6655d03abc621
VScdr <shubin.va@phystech.edu>
2023-10-16 19:28:46 +0300

    Add inplaceRLE

---
+++ FUN/RLE.cpp
@@ -0,0 +1,21 @@
+template<typename T>
+long RLE(std::vector<T>& str) {
+  long ins_pos = 0;
+  for (int i = 0; i < std::ssize(str); ++i) {
+    auto ch = str[i];
+    int cnt = 1;
+    for (; i + 1 < std::ssize(str) && ch == str[i + 1]; ++i) {
+      ++cnt;
+    }
+    if (cnt > 1) {
+      auto count = std::to_string(cnt);
+      std::ranges::copy(count, str.begin() + ins_pos);
+      str[ins_pos + std::ssize(count)] = ch;
+      ins_pos += std::ssize(count);
+    } else {
+      str[ins_pos] = ch;
+    }
+    ++ins_pos;
+  }
+  return ins_pos;
+}
