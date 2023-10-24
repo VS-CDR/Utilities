cbdd3df4c146087b42e0bc4647379d47a959b181
VScdr <shubin.va@phystech.edu>
2023-10-25 00:33:06 +0300

    Update RLE.cpp
    refactor code

--- FUN/RLE.cpp
+++ FUN/RLE.cpp
@@ -1,21 +1,15 @@
 template<typename T>
-long inplaceRLE(std::vector<T>& str) {
+long InplaceRLE(std::vector<T>& str) {
   long ins_pos = 0;
-  for (int i = 0; i < std::ssize(str); ++i) {
-    auto ch = str[i];
+  for (int i = 0; i < std::ssize(str); ++i, ++ins_pos) {
     int cnt = 1;
-    for (; i + 1 < std::ssize(str) && ch == str[i + 1]; ++i) {
-      ++cnt;
-    }
+    for (; i + 1 < std::ssize(str) && str[i] == str[i + 1]; ++i) { ++cnt; }
     if (cnt > 1) {
       auto count = std::to_string(cnt);
       std::ranges::copy(count, str.begin() + ins_pos);
-      str[ins_pos + std::ssize(count)] = ch;
       ins_pos += std::ssize(count);
-    } else {
-      str[ins_pos] = ch;
     }
-    ++ins_pos;
+    str[ins_pos] = str[i];
   }
   return ins_pos;
 }
@@ -23,12 +17,11 @@ long inplaceRLE(std::vector<T>& str) {
 std::string RLE(std::string_view str) {
   std::string res;
   for (int i = 0; i < std::ssize(str); ++i) {
-    auto ch = str[i];
+    res += str[i];
     int cnt = 1;
-    for (; i + 1 < std::ssize(str) && ch == str[i + 1]; ++i) {
+    for (auto ch = str[i]; i + 1 < std::ssize(str) && ch == str[i + 1]; ++i) {
       ++cnt;
     }
-    res += ch;
     res += std::to_string(cnt);
   }
   return res;
