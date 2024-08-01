685307d17af1b4b4dd4ac37d519364dbf635bf7f
VScdr <shubin.va@phystech.edu>
2024-08-01 21:26:16 +0300

    Update BinarySearchesCheat.cpp

--- FUN/BinarySearchesCheat.cpp
+++ FUN/BinarySearchesCheat.cpp
@@ -1,7 +1,9 @@
+#include <compare>
 #include <numeric>
 #include <vector>
 
-ssize_t lower_bound(std::vector<int> arr, int val) {
+template <std::three_way_comparable T>
+ssize_t lower_bound(std::vector<T> arr, T val) {
   ssize_t l = -1, r = std::ssize(arr);
   while (l + 1 < r) {
     auto mid = std::midpoint(l, r);
@@ -10,7 +12,8 @@ ssize_t lower_bound(std::vector<int> arr, int val) {
   return r;
 }
 
-ssize_t upper_bound(std::vector<int> arr, int val) {
+template <std::three_way_comparable T>
+ssize_t upper_bound(std::vector<T> arr, T val) {
   ssize_t l = -1, r = std::ssize(arr);
   while (l + 1 < r) {
     auto mid = std::midpoint(l, r);
