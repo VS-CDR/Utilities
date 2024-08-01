fe15bad459bff7ab0ec58b1f96d664a4c029e7cf
VScdr <shubin.va@phystech.edu>
2024-08-01 21:18:51 +0300

    Update BinarySearchesCheat.cpp

--- FUN/BinarySearchesCheat.cpp
+++ FUN/BinarySearchesCheat.cpp
@@ -1,5 +1,8 @@
-long lower_bound(std::vector<int> arr, int val) {
-  long l = -1, r = std::ssize(arr);
+#include <numeric>
+#include <vector>
+
+ssize_t lower_bound(std::vector<int> arr, int val) {
+  ssize_t l = -1, r = std::ssize(arr);
   while (l + 1 < r) {
     auto mid = std::midpoint(l, r);
     (arr[mid] < val ? l : r) = mid;
@@ -7,8 +10,8 @@ long lower_bound(std::vector<int> arr, int val) {
   return r;
 }
 
-long upper_bound(std::vector<int> arr, int val) {
-  long l = -1, r = std::ssize(arr);
+ssize_t upper_bound(std::vector<int> arr, int val) {
+  ssize_t l = -1, r = std::ssize(arr);
   while (l + 1 < r) {
     auto mid = std::midpoint(l, r);
     (arr[mid] <= val ? l : r) = mid;
