c224a3844747f4a2eeac23f54422e391b803dac7
VScdr <shubin.va@phystech.edu>
2024-08-17 23:34:26 +0300

    Update decompressor.cpp

--- Coders & Decoders/EliasDecoder/decompressor.cpp
+++ Coders & Decoders/EliasDecoder/decompressor.cpp
@@ -1,7 +1,8 @@
 #include <algorithm>
-#include <vector>
-#include <string>
 #include <iostream>
+#include <iterator>
+#include <string>
+#include <vector>
 
 int Transfer(int byte, int radix) {
   int res = 0;
@@ -11,24 +12,22 @@ int Transfer(int byte, int radix) {
   return res;
 }
 
-void Decompress(std::vector<std::string>& dec, int arg) {
+void Decompress(std::vector<std::string> &dec, int arg) {
   for (auto i = std::ssize(dec) - 1; i >= 0; --i, arg ^= 1) {
     auto cnt = std::stoi(dec[i]);
     dec[i].clear();
-    for (int j = 0; j < cnt; ++j) {
-      dec[i].push_back('0' + arg);
-    }
+    std::fill_n(std::back_inserter(dec[i]), cnt, '0' + arg);
   }
 }
 
-void DeleteNull(std::string& str) {
+void DeleteNull(std::string &str) {
   int cnt = 0;
   for (int i = 0; i < std::ssize(str) && str[i] != '\0'; ++i) {
     if (str[i] == '0') {
       ++cnt;
       str.erase(i--, 1);
     } else if (cnt != 0) {
-      i += cnt + 1;
+      i += ++cnt;
       str.insert(i, " ");
       cnt = 0;
     } else {
@@ -44,7 +43,7 @@ decltype(auto) InDEC(std::string_view str) {
       int combo = 0;
       int pow = 0;
       for (auto j = i - 1; j >= 0 && str[j] != ' '; --j) {
-        combo += (str[j] - '0') * (1 << pow++);
+        combo += static_cast<int>(str[j] - '0') * (1 << pow++);
       }
       res.emplace_back(std::to_string(combo));
     }
@@ -52,10 +51,10 @@ decltype(auto) InDEC(std::string_view str) {
   return res;
 }
 
-void DecodeAndOtput(const std::vector<std::string>& dec) {
+void DecodeAndOtput(const std::vector<std::string> &dec) {
   std::string res;
   std::string symb;
-  std::ranges::for_each(dec, [&res](const auto& elem) { res += elem; });
+  std::ranges::for_each(dec, [&res](const auto &elem) { res += elem; });
   auto is_full = std::ssize(res) % 8;
   if (is_full != 0) {
     auto cnt_nan = is_full - 1;
@@ -77,14 +76,14 @@ void DecodeAndOtput(const std::vector<std::string>& dec) {
   std::cout << std::endl;
 }
 
-void InputCode(std::string& code) {
+void InputCode(std::string &code) {
   std::cout << "Input your code: ";
   std::getline(std::cin, code);
 }
 
-void RemoveSpaces(std::string& str);
+void RemoveSpaces(std::string &str);
 
-int FirstBit(std::string& code) {
+int FirstBit(std::string &code) {
   int first_bit = code[0] == '0' ? 0 : 1;
   code.erase(0, 1);
   return first_bit;
@@ -102,7 +101,7 @@ int main() {
   DecodeAndOtput(dec);
 }
 
-void RemoveSpaces(std::string& str) {
+void RemoveSpaces(std::string &str) {
   int ins_pos = 0;
   for (auto ch : str) {
     if (ch != ' ') {
